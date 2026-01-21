3db67819af10eedfb48e54d2adbe4517c7744b58
AsgardF <furqanmaulidi8@gmail.com>
2026-01-21 21:38:02 +0700

    Fix bug Navbar name

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Why from './components/Why'
 import Pricing from './components/Pricing'
